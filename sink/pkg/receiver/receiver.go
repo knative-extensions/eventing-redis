7b29063f318531c1ec1fe3e588661e4ebe7c70cd
Ansu Varghese <avarghese@us.ibm.com>
2021-02-12 18:27:05 -0500

    Updates for RedisStreamSink: cloud based Redis instance, minor fixes, documentation cleanup etc
    Signed-off-by: Ansu Varghese <avarghese@us.ibm.com>

--- sink/pkg/receiver/receiver.go
+++ sink/pkg/receiver/receiver.go
@@ -18,6 +18,8 @@ package receiver
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
@@ -25,6 +27,8 @@ import (
 	"go.uber.org/zap"
 	"knative.dev/eventing/pkg/adapter/v2"
 	"knative.dev/pkg/logging"
+
+	redisParse "github.com/go-redis/redis/v8"
 )
 
 type Receiver interface {
@@ -45,13 +49,13 @@ func NewReceiver(ctx context.Context, processed adapter.EnvConfigAccessor) Recei
 	config := processed.(*Config)
 	return &receiver{
 		config: config,
-		pool:   newPool(config.Address),
+		pool:   newPool(config.Address, config.TLSCertificate),
 		logger: logging.FromContext(ctx).Desugar().With(zap.String("stream", config.Stream)),
 	}
 }
 
 func (r *receiver) Receive(event cloudevents.Event) {
-	r.logger.Info("receiving event", zap.Any("event", event))
+	r.logger.Info("Receiving event", zap.Any("event", event))
 	conn, _ := r.pool.Dial()
 	defer conn.Close()
 
@@ -59,20 +63,26 @@ func (r *receiver) Receive(event cloudevents.Event) {
 	var fields []interface{}
 	err := json.Unmarshal(event.Data(), &fields)
 	if err != nil {
-		r.logger.Error("cannot decode event", zap.Error(err))
+		r.logger.Error("Cannot decode event", zap.Error(err))
 		return
 	}
+
 	args := []interface{}{r.config.Stream, "*"}
 	args = append(args, fields...)
 
 	_, err = conn.Do("XADD", args...)
-
 	if err != nil {
-		r.logger.Error("cannot write to stream", zap.Error(err))
+		r.logger.Error("Cannot write to stream", zap.Error(err))
 	}
+	r.logger.Info("Added event to the stream")
+
 }
 
-func newPool(address string) *redis.Pool {
+func newPool(address string, tlscert string) *redis.Pool {
+	opt, err := redisParse.ParseURL(address)
+	if err != nil {
+		panic(err)
+	}
 	return &redis.Pool{
 		// Maximum number of idle connections in the pool.
 		MaxIdle: 80,
@@ -81,9 +91,30 @@ func newPool(address string) *redis.Pool {
 		// Dial is an application supplied function for creating and
 		// configuring a connection.
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", address)
-			if err != nil {
-				panic(err.Error())
+			var c redis.Conn
+			if opt.Password != "" && tlscert != "" {
+				roots := x509.NewCertPool()
+				ok := roots.AppendCertsFromPEM([]byte(tlscert))
+				if !ok {
+					panic(err)
+				}
+				c, err = redis.Dial("tcp", opt.Addr,
+					//redis.DialUsername(opt.Username), //username needs to be empty for successful redis connection (v8 go-redis issue)
+					redis.DialPassword(opt.Password),
+					redis.DialTLSConfig(&tls.Config{
+						RootCAs: roots,
+					}),
+					redis.DialTLSSkipVerify(true),
+					redis.DialUseTLS(true),
+				)
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				c, err = redis.Dial("tcp", opt.Addr)
+				if err != nil {
+					panic(err)
+				}
 			}
 			return c, err
 		},
