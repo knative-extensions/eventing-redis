5ae6265adc89a4d754fe4267761a1c265cfc5f74
Knative Automation <automation@knative.team>
2021-08-19 02:11:51 +0000

    upgrade to latest dependencies
    bumping knative.dev/eventing 0c41e6d...4b18ab8:
      > 4b18ab8 Add myself to support roster (# 5640)
      > 5c08bd3 be more explicit about the semantic of ceOverrides (# 5641)
      > c183833 Resurrect source spec (# 5637)
      > 72fae58 Add trace exporting to mt broker (# 5634)
      > 41e36ee Update community files (# 5632)
      > 6296a81 Add tracing support to eventdisplay and heartbeats (# 5631)
    bumping knative.dev/pkg 21eb4c1...7b5ecbc:
      > 7b5ecbc Sketch out an alternate way of injecting clients/informers (# 2210)
      > 8c88fa2 Revert "Remove the unused file (# 1935)" (# 2213)
      > 50410e0 Use consistent case for "Deprecated" comments (# 2216)
      > 35bcd16 Drop redundant pointers and decoders (# 2211)
      > bf176d5 Sink two codegen passes below loop. (# 2209)
      > 2c8a7b6 Update community files (# 2208)
      > 23b0147 Update actions (# 2206)
    bumping knative.dev/serving 5ad15df...265dfe5:
      > 265dfe5 Update net-istio nightly (# 11825)
      > 0629672 Update net-istio nightly (# 11816)
      > fa213a4 Update net-certmanager nightly (# 11815)
      > 3e7900e Remove deprecated ServiceName field (# 11817)
      > 6a54ed8 Adds a flag to skip conformance tests against non-required API (# 11769)
      > 32f12b6 Makes conformance test for error condition more genernal (# 11784)
      > b7abe27 Update net-istio nightly (# 11808)
      > 269555f Update net-kourier nightly (# 11807)
      > 3bf25ce Update net-kourier nightly (# 11804)
      > db6e59d Update net-istio nightly (# 11803)
      > 1f7f69c Update community files (# 11800)
    bumping knative.dev/hack e28525d...815cd31:
      > 815cd31 Stop performing pre-releases (# 77)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- sink/pkg/client/injection/informers/sinks/v1alpha1/redisstreamsink/redisstreamsink.go
+++ sink/pkg/client/injection/informers/sinks/v1alpha1/redisstreamsink/redisstreamsink.go
@@ -21,8 +21,15 @@ package redisstreamsink
 import (
 	context "context"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	cache "k8s.io/client-go/tools/cache"
+	apissinksv1alpha1 "knative.dev/eventing-redis/sink/pkg/apis/sinks/v1alpha1"
+	versioned "knative.dev/eventing-redis/sink/pkg/client/clientset/versioned"
 	v1alpha1 "knative.dev/eventing-redis/sink/pkg/client/informers/externalversions/sinks/v1alpha1"
+	client "knative.dev/eventing-redis/sink/pkg/client/injection/client"
 	factory "knative.dev/eventing-redis/sink/pkg/client/injection/informers/factory"
+	sinksv1alpha1 "knative.dev/eventing-redis/sink/pkg/client/listers/sinks/v1alpha1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -30,6 +37,7 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
+	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -41,6 +49,11 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
+func withDynamicInformer(ctx context.Context) context.Context {
+	inf := &wrapper{client: client.Get(ctx)}
+	return context.WithValue(ctx, Key{}, inf)
+}
+
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1alpha1.RedisStreamSinkInformer {
 	untyped := ctx.Value(Key{})
@@ -49,4 +62,45 @@ func Get(ctx context.Context) v1alpha1.RedisStreamSinkInformer {
 			"Unable to fetch knative.dev/eventing-redis/sink/pkg/client/informers/externalversions/sinks/v1alpha1.RedisStreamSinkInformer from context.")
 	}
 	return untyped.(v1alpha1.RedisStreamSinkInformer)
+}
+
+type wrapper struct {
+	client versioned.Interface
+
+	namespace string
+}
+
+var _ v1alpha1.RedisStreamSinkInformer = (*wrapper)(nil)
+var _ sinksv1alpha1.RedisStreamSinkLister = (*wrapper)(nil)
+
+func (w *wrapper) Informer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(nil, &apissinksv1alpha1.RedisStreamSink{}, 0, nil)
+}
+
+func (w *wrapper) Lister() sinksv1alpha1.RedisStreamSinkLister {
+	return w
+}
+
+func (w *wrapper) RedisStreamSinks(namespace string) sinksv1alpha1.RedisStreamSinkNamespaceLister {
+	return &wrapper{client: w.client, namespace: namespace}
+}
+
+func (w *wrapper) List(selector labels.Selector) (ret []*apissinksv1alpha1.RedisStreamSink, err error) {
+	lo, err := w.client.SourcesV1alpha1().RedisStreamSinks(w.namespace).List(context.TODO(), v1.ListOptions{
+		LabelSelector: selector.String(),
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
+	if err != nil {
+		return nil, err
+	}
+	for idx := range lo.Items {
+		ret = append(ret, &lo.Items[idx])
+	}
+	return ret, nil
+}
+
+func (w *wrapper) Get(name string) (*apissinksv1alpha1.RedisStreamSink, error) {
+	return w.client.SourcesV1alpha1().RedisStreamSinks(w.namespace).Get(context.TODO(), name, v1.GetOptions{
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
 }
--- source/pkg/client/injection/informers/sources/v1alpha1/redisstreamsource/redisstreamsource.go
+++ source/pkg/client/injection/informers/sources/v1alpha1/redisstreamsource/redisstreamsource.go
@@ -21,8 +21,15 @@ package redisstreamsource
 import (
 	context "context"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	cache "k8s.io/client-go/tools/cache"
+	apissourcesv1alpha1 "knative.dev/eventing-redis/source/pkg/apis/sources/v1alpha1"
+	versioned "knative.dev/eventing-redis/source/pkg/client/clientset/versioned"
 	v1alpha1 "knative.dev/eventing-redis/source/pkg/client/informers/externalversions/sources/v1alpha1"
+	client "knative.dev/eventing-redis/source/pkg/client/injection/client"
 	factory "knative.dev/eventing-redis/source/pkg/client/injection/informers/factory"
+	sourcesv1alpha1 "knative.dev/eventing-redis/source/pkg/client/listers/sources/v1alpha1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -30,6 +37,7 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
+	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -41,6 +49,11 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
+func withDynamicInformer(ctx context.Context) context.Context {
+	inf := &wrapper{client: client.Get(ctx)}
+	return context.WithValue(ctx, Key{}, inf)
+}
+
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1alpha1.RedisStreamSourceInformer {
 	untyped := ctx.Value(Key{})
@@ -49,4 +62,45 @@ func Get(ctx context.Context) v1alpha1.RedisStreamSourceInformer {
 			"Unable to fetch knative.dev/eventing-redis/source/pkg/client/informers/externalversions/sources/v1alpha1.RedisStreamSourceInformer from context.")
 	}
 	return untyped.(v1alpha1.RedisStreamSourceInformer)
+}
+
+type wrapper struct {
+	client versioned.Interface
+
+	namespace string
+}
+
+var _ v1alpha1.RedisStreamSourceInformer = (*wrapper)(nil)
+var _ sourcesv1alpha1.RedisStreamSourceLister = (*wrapper)(nil)
+
+func (w *wrapper) Informer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(nil, &apissourcesv1alpha1.RedisStreamSource{}, 0, nil)
+}
+
+func (w *wrapper) Lister() sourcesv1alpha1.RedisStreamSourceLister {
+	return w
+}
+
+func (w *wrapper) RedisStreamSources(namespace string) sourcesv1alpha1.RedisStreamSourceNamespaceLister {
+	return &wrapper{client: w.client, namespace: namespace}
+}
+
+func (w *wrapper) List(selector labels.Selector) (ret []*apissourcesv1alpha1.RedisStreamSource, err error) {
+	lo, err := w.client.SourcesV1alpha1().RedisStreamSources(w.namespace).List(context.TODO(), v1.ListOptions{
+		LabelSelector: selector.String(),
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
+	if err != nil {
+		return nil, err
+	}
+	for idx := range lo.Items {
+		ret = append(ret, &lo.Items[idx])
+	}
+	return ret, nil
+}
+
+func (w *wrapper) Get(name string) (*apissourcesv1alpha1.RedisStreamSource, error) {
+	return w.client.SourcesV1alpha1().RedisStreamSources(w.namespace).Get(context.TODO(), name, v1.GetOptions{
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
 }
